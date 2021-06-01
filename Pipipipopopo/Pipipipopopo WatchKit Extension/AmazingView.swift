a99c69d84549e6759f6ed598b28795235f50f63f
Palloma Ramos <pallomaramos@MacBook-Pro-de-Palloma.local>
2021-06-01 14:57:58 -0300

    Elementos principais Tela Amazing

---
+++ Pipipipopopo/Pipipipopopo WatchKit Extension/AmazingView.swift
@@ -0,0 +1,38 @@
+//
+//  AmazingView.swift
+//  Pipipipopopo WatchKit Extension
+//
+//  Created by Palloma Ramos on 01/06/21.
+//
+
+import SwiftUI
+import WatchKit
+
+struct AmazingView: View {
+    
+    var body: some View {
+        VStack{
+           Text("AMAZING!")
+            Text("Você completou o treino")
+                .font(.system(size: 14))
+            Image("Amazing")
+                .padding()
+            Button("menu") {
+                print("Button tapped!")
+            }.foregroundColor(.black)
+            .background(Color(" ButtonColor"))
+            .cornerRadius(10)
+            
+            
+        }
+    }
+    
+}
+
+struct AmazingView_Previews: PreviewProvider {
+    static var previews: some View {
+        AmazingView()
+    }
+}
+
+
