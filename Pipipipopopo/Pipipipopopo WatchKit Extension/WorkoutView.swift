7f965df4d39b6d8a07380e49405338434ce72ee1
Morgana Galamba <mbfg2@cin.ufpe.br>
2021-06-02 15:04:43 -0300

    sound playing once

--- Pipipipopopo/Pipipipopopo WatchKit Extension/WorkoutView.swift
+++ Pipipipopopo/Pipipipopopo WatchKit Extension/WorkoutView.swift
@@ -15,48 +15,74 @@ struct WorkoutView: View {
     @State var seconds: Int = 10
     @State var zeroMinutes: String = ""
     @State var zeroSeconds: String = ""
+    @State var series: Int = 2
+    @State var isTiming: Bool = true
+    @State var exerciseCount: Int = 5
+    @State var exerciseName: String = "Polichinelo"
+    @State var exercises: Int = 2
+    @State var timer: Timer?
     
     var body: some View {
         VStack{
-            Text("\(zeroMinutes)\(minutes):\(zeroSeconds)\(seconds)")
-                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                .onAppear(){
-                    if self.minutes < 10 {
-                        self.zeroMinutes = "0"
-                    } else {
-                        self.zeroMinutes = ""
-                    }
-                    if self.seconds < 10 {
-                        self.zeroSeconds = "0"
-                    }
-                    Timer.scheduledTimer(withTimeInterval: 1, repeats: true){ _ in
-                        if self.minutes > 0 {
-                            if self.seconds > 0 {
-                                self.seconds -= 1
+            
+            if exercises > 0 {
+                
+                if isTiming {
+                    Text("\(zeroMinutes)\(minutes):\(zeroSeconds)\(seconds)")
+                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                        .onAppear(){
+                            if self.minutes < 10 {
+                                self.zeroMinutes = "0"
                             } else {
-                                self.seconds = 59
-                                self.minutes -= 1
-                                self.zeroSeconds = ""
+                                self.zeroMinutes = ""
                             }
-                        } else if self.minutes == 0 {
-                            if self.seconds > 0 {
-                                self.seconds -= 1
+                            if self.seconds < 10 {
+                                self.zeroSeconds = "0"
+                            }
+                            self.timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true){ _ in
+                                if self.minutes > 0 {
+                                    if self.seconds > 0 {
+                                        self.seconds -= 1
+                                    } else {
+                                        self.seconds = 59
+                                        self.minutes -= 1
+                                        self.zeroSeconds = ""
+                                    }
+                                } else if self.minutes == 0 {
+                                    if self.seconds > 0 {
+                                        self.seconds -= 1
+                                    }
+                                }
+                                if self.minutes < 10 {
+                                    self.zeroMinutes = "0"
+                                } else {
+                                    self.zeroMinutes = ""
+                                }
+                                if self.seconds < 10 {
+                                    self.zeroSeconds = "0"
+                                }
+                                
+                                if self.seconds == 0 && self.minutes == 0 {
+                                    
+                                    
+                                    WKInterfaceDevice.current().play(.stop)
+                                    exercises -= 1
+                                    isTiming.toggle()
+                                }
                             }
                         }
-                        if self.minutes < 10 {
-                            self.zeroMinutes = "0"
-                        } else {
-                            self.zeroMinutes = ""
-                        }
-                        if self.seconds < 10 {
-                            self.zeroSeconds = "0"
-                        }
-                        
-                        if self.seconds == 0 && self.minutes == 0 {
-                            WKInterfaceDevice.current().play(.stop)
-                        }
-                    }
                 }
+                else { // exercicio de contagem
+                    Text("\(exerciseCount)")
+                        .onAppear(){
+                            print("entrou")
+                            self.timer?.invalidate()
+                            self.timer = nil
+                        }
+                    //print("entrou")
+                    
+                }
+            }
             Image("Chart")
                 .padding()
             
